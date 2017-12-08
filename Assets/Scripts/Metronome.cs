a4eb45eabd3a5beb01f3ba753696a91616eed253
Marcus Guimaraes <mguimaraes42@gmail.com>
2017-12-07 21:33:19 -0500

    init repo

---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour {
+
+	private Beat.Clock clock;
+	private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		clock = Beat.Clock.Instance;
+		source = GetComponent<AudioSource>();
+		clock.Beat += Tick;
+	}
+
+	void Tick(Beat.Clock.BeatArgs args) {
+		source.Play();
+	}
+
+	void OnDestroy() {
+		clock.Beat -= Tick;
+	}
+}
